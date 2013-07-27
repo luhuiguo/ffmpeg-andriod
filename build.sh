0031eb0b067d694fea947395959b8ab51e3e4972
Lu Huiguo <luhuiguo@gmail.com>
2013-07-27 11:56:49 +0800

    .

--- build.sh
+++ build.sh
@@ -13,13 +13,15 @@ fi
 DEST=`pwd`/build/ffmpeg && rm -rf $DEST
 SOURCE=`pwd`/ffmpeg
 
-PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt/darwin-x86_64
-SYSROOT=$NDK/platforms/android-9/arch-arm
+PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64
+SYSROOT=$NDK/platforms/android-18/arch-arm
 
 export PATH=$PREBUILT/bin:$PATH
 export CC=$PREBUILT/bin/arm-linux-androideabi-gcc
 export LD=$PREBUILT/bin/arm-linux-androideabi-ld
 export AR=$PREBUILT/bin/arm-linux-androideabi-ar
+export RANLIB=$PREBUILT/bin/arm-linux-androideabi-ranlib
+export STRIP=$PREBUILT/bin/arm-linux-androideabi-strip
 
 CFLAGS="-O3 -Wall -mthumb -pipe -fpic -fasm \
   -finline-limit=300 -ffast-math \
@@ -30,7 +32,7 @@ CFLAGS="-O3 -Wall -mthumb -pipe -fpic -fasm \
 
 FFMPEG_FLAGS="--target-os=linux \
   --sysroot=$SYSROOT \
-  --cc=$PREBUILT/bin/arm-linux-androideabi-gcc
+  --cc=$CC \
   --arch=arm \
   --enable-cross-compile \
   --cross-prefix=arm-linux-androideabi- \
@@ -59,17 +61,17 @@ FFMPEG_FLAGS="--target-os=linux \
 
 
 for version in neon armv7 vfp armv6; do
-
+#for version in neon; do
   cd $SOURCE
 
   case $version in
     neon)
       EXTRA_CFLAGS="-march=armv7-a -mfpu=neon -mfloat-abi=softfp -mvectorize-with-neon-quad"
-      EXTRA_LDFLAGS="-Wl,--fix-cortex-a8"
+      EXTRA_LDFLAGS=""
       ;;
     armv7)
       EXTRA_CFLAGS="-march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp"
-      EXTRA_LDFLAGS="-Wl,--fix-cortex-a8"
+      EXTRA_LDFLAGS=""
       ;;
     vfp)
       EXTRA_CFLAGS="-march=armv6 -mfpu=vfp -mfloat-abi=softfp"
@@ -96,13 +98,17 @@ for version in neon armv7 vfp armv6; do
   make -j4 || exit 1
   make install || exit 1
   
-  $PREBUILT/bin/arm-linux-androideabi-ar d libavcodec/libavcodec.a inverse.o  
-  $PREBUILT/bin/arm-linux-androideabi-ar -dv libavcodec/libavcodec.a log2_tab.o
-  $PREBUILT/bin/arm-linux-androideabi-ar -dv libavutil/libavutil.a log2_tab.o
+  $AR d libavcodec/libavcodec.a inverse.o  
+  $AR d libavcodec/libavcodec.a log2_tab.o
+  $AR d libavutil/libavutil.a log2_tab.o
   
-  $PREBUILT/bin/arm-linux-androideabi-ld -rpath-link=$SYSROOT/usr/lib -L$SYSROOT/usr/lib  -soname libffmpeg.so -shared -nostdlib  -z,noexecstack -Bsymbolic --whole-archive --no-undefined -o $PREFIX/libffmpeg.so libavcodec/libavcodec.a libavformat/libavformat.a libavutil/libavutil.a libswscale/libswscale.a -lc -lm -lz -ldl -llog  --warn-once  --dynamic-linker=/system/bin/linker $PREBUILT/lib/gcc/arm-linux-androideabi/4.4.3/libgcc.a  
+  $LD -rpath-link=$SYSROOT/usr/lib -L$SYSROOT/usr/lib \
+   -soname libffmpeg.so -shared -nostdlib -z noexecstack \
+   -Bsymbolic --whole-archive --no-undefined \
+   -o $PREFIX/libffmpeg.so libavcodec/libavcodec.a libavformat/libavformat.a libavutil/libavutil.a libswscale/libswscale.a \
+   -lc -lm -lz -ldl -llog --dynamic-linker=/system/bin/linker $PREBUILT/lib/gcc/arm-linux-androideabi/4.8/libgcc.a  
 
   cp $PREFIX/libffmpeg.so $PREFIX/libffmpeg-debug.so
-  $PREBUILT/bin/arm-linux-androideabi-strip --strip-unneeded $PREFIX/libffmpeg.so
+  $STRIP --strip-unneeded $PREFIX/libffmpeg.so
 
 done
